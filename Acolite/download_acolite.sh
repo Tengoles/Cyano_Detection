4ab9f2ae8aa9c3d06cb6e06c10ac7a6dbc3ab895
Enzo <enzo.tng@gmail.com>
2021-03-27 22:44:44 +0000

    download official repo too

--- Acolite/download_acolite.sh
+++ Acolite/download_acolite.sh
@@ -1,1 +1,1 @@
-wget https://odnature.naturalsciences.be/downloads/remsem/acolite/acolite_py_linux_20210114.0.tar.gz ; tar -xf acolite_py_linux_20210114.0.tar.gz
+wget https://odnature.naturalsciences.be/downloads/remsem/acolite/acolite_py_linux_20210114.0.tar.gz ; tar -xf acolite_py_linux_20210114.0.tar.gz ; git clone https://github.com/acolite/acolite_ls2 acolite
